47ad882c931c5c9de09245f262fd84f22eed7567
Orphey <15167344+frostmorn@users.noreply.github.com>
2022-07-09 03:29:29 +0000

    Implemented multiple request through single connection

--- HTTPRequest.h
+++ HTTPRequest.h
@@ -1,22 +1,31 @@
 #ifndef HTTP_REQUEST_H
 #define HTTP_REQUEST_H
+#include "defines.h"
 #include <string>
+#include <iostream>
 class HTTPConnection;
 class HTTPRequest
 {
 private:
-    /* data */
-    HTTPConnection *conn;
-    std::string req_data;
+  /* data */
+  HTTPConnection *conn;
+  std::string req_data;
+  void ParseHeaders();
 public:
-    HTTPRequest(HTTPConnection *conn, std::string &req_data);
-    ~HTTPRequest();
+  HTTPRequest(HTTPConnection *conn, std::string &req_data);
+  ~HTTPRequest();
 };
 
 HTTPRequest::HTTPRequest(HTTPConnection *conn, std::string &req_data)
 {
-    this->conn = conn;   
-    this->req_data = req_data; 
+  std::cout << SINFO_PREFIX << "HTTPRequest created" << std::endl;
+  this->conn = conn;   
+  this->req_data = req_data; 
+}
+
+void HTTPRequest::ParseHeaders(){
+
+
 }
 
 HTTPRequest::~HTTPRequest()
